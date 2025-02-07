7e5dece084c4d866687f2061e90cc448bc172d66
collenkim <sasin8712@gmail.com>
2025-02-07 14:13:37 +0900

    Initial commit

---
+++ src/test/java/com/collenkim/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collenkim.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
